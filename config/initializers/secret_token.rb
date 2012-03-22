1fdb316931eea043a7179a5be9734265768760a8
Your Name <youremail@example.com>
2012-03-22 15:08:20 -0700

    Done

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testit::Application.config.secret_token = 'd0d401c0854cd70db2d881f1e34f83af1bddc327384ad718320c0c44798c2a94339700bffb8f4ce5ccef360b2ad6182c60668863fc2789c2e91093dd884c13b4'
